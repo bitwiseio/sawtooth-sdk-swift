bcb4fc635363baed11d3480164222724883ec647
Darian Plumb <dplumb@bitwise.io>
2018-11-13 16:19:29 -0600

    Add XO iOS app skeleton
    Also includes a .gitignore file to ignore xcode user settings and
    build artifacts.
    
    Signed-off-by: Darian Plumb <dplumb@bitwise.io>

---
+++ examples/xo_ios/xo_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xo_ios
+//
+//  Created by Darian Plumb on 11/13/18.
+//  Copyright © 2018 Bitwise IO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
