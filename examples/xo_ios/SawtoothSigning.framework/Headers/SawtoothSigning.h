32d01d5572503984ca53d66cc5dc50712d0d1781
Shannyn Telander <telander@bitwise.io>
2018-11-27 09:43:44 -0600

    Add SawtoothSigning framework to xo_ios project
    In order to use the SawtoothSigning framework, a built version of this framework is included in the project directory so that the system can be easily tested directly from the contents of the repository. This framework is being included as an embedded binary within the project.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ examples/xo_ios/SawtoothSigning.framework/Headers/SawtoothSigning.h
@@ -0,0 +1,29 @@
+//
+//  SawtoothSigning.h
+//  SawtoothSigning
+//
+//  Copyright 2018 Bitwise IO, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import <UIKit/UIKit.h>
+#import <CommonCrypto/CommonHMAC.h>
+
+
+//! Project version number for SawtoothSigning.
+FOUNDATION_EXPORT double SawtoothSigningVersionNumber;
+
+//! Project version string for SawtoothSigning.
+FOUNDATION_EXPORT const unsigned char SawtoothSigningVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SawtoothSigning/PublicHeader.h>
