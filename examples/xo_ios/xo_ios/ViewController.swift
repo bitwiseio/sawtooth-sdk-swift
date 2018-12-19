ee31275ff846b910ebae1a94421407fcdf82f73a
Eloá Franca Verona <eloafran@bitwise.io>
2018-12-19 13:11:19 -0600

    Add table view to display list of games
    This commits implements a simple list view of all the games in state
    
    Signed-off-by: Eloá França Verona <eloafran@bitwise.io>
    Co-Authored-By: Darian Plumb <dplumb@bitwise.io>

--- examples/xo_ios/xo_ios/ViewController.swift
+++ examples/xo_ios/xo_ios/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: Properties
+    var games = [Game]()
     let gameHandler = XORequestHandler()
 
     override func viewDidLoad() {
@@ -21,6 +23,7 @@ class ViewController: UIViewController {
                                                name: UserDefaults.didChangeNotification,
                                                object: nil)
         settingsChanged()
+        loadGames()
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -40,5 +43,58 @@ class ViewController: UIViewController {
         } else {
             gameHandler.setUrl(url: "http://localhost:8080")
         }
+    }
+
+    private func loadGames() {
+        var games: [[String: Any]] = []
+        var gameList: [Game] = []
+        self.games.append(Game(name: "String",
+                               board: "String",
+                               gameState: "P1-NEXT",
+                               playerKey1: "String",
+                               playerKey2: "String"))
+        gameHandler.listGames(completion: {response in
+            games = response["data"] as? [[String: Any]] ?? []
+            for game in games {
+                // swiftlint:disable force_cast
+                let base64Encoded = game["data"]! as! String
+                gameList.append(self.parseGames(data: base64Encoded))
+            }
+            self.games = gameList
+            self.games.sort(by: { (game1, game2) -> Bool in
+                game1.name < game2.name
+            })
+            self.tableView.reloadData()
+        })
+    }
+
+    private func parseGames(data: String) -> Game {
+        let decodedData = Data(base64Encoded: data)!
+        let split = String(data: decodedData, encoding: .utf8)!.components(separatedBy: ",")
+        return Game(name: split[0], board: split[1], gameState: split[2], playerKey1: split[3], playerKey2: split[4])
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cellIdentifier = "GameTableViewCell"
+        guard let cell =
+            tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? GameTableViewCell  else {
+                fatalError("The dequeued cell is not an instance of GameTableViewCell.")
+        }
+
+        let game = games[indexPath.row]
+        cell.gameNameLabel.text = game.name
+        cell.gameStateLabel.text = game.gameState?.rawValue
+        return cell
     }
 }
